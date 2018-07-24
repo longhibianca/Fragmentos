dc5d0a9d78ce9f82f368ec0de07f5ecae0d50e8f
Bianca Longhi <longhisbianca@gmail.com>
2018-07-23 23:01:37 -0300

    usando fragmentos dinamicos. Quando clica no primeiro btn abre o primeiro fragmento, quando clica no segundo btn, abre o segundo fragmento

--- app/src/main/java/br/edu/iff/pooa20181/fragmentos/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/fragmentos/MainActivity.java
@@ -2,6 +2,8 @@ package br.edu.iff.pooa20181.fragmentos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //quando cria a activity, mostra o primeiro fragmento
+        if (savedInstanceState == null) {
+        // adicionar o fragmento inicial
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.frame_container, new PrimeiroFragment())
+                    .commit();
+
+        }
+        //pega a referencia dos botões
+        Button f1 = (Button) findViewById(R.id.btnFrag1);
+        Button f2 = (Button) findViewById(R.id.btnFrag2);
+
+        //quando clicar no btn 1 abre o primeiro fragmento
+        f1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_container, new PrimeiroFragment()).commit();
+
+            }
+        });
+
+        //quando clcicar no segundo btn, abre o segundo fragmento
+        f2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_container, new SegundoFragment()).commit();
+
+            }
+        });
     }
 }
