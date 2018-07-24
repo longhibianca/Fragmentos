937376278f4b7f30bccb5e3d5e7206a57b05704e
Bianca Longhi <longhisbianca@gmail.com>
2018-07-23 23:55:36 -0300

    tratando botão do primeiro fragmento

--- app/src/main/java/br/edu/iff/pooa20181/fragmentos/PrimeiroFragment.java
+++ app/src/main/java/br/edu/iff/pooa20181/fragmentos/PrimeiroFragment.java
@@ -6,13 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PrimeiroFragment extends Fragment {
-
+    View view;
 
     public PrimeiroFragment() {
         // Required empty public constructor
@@ -22,8 +24,24 @@ public class PrimeiroFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_primeiro, container, false);
+
+        // obter a view do fragmento
+        view = inflater.inflate(R.layout.fragment_primeiro, container, false);
+        // linkar com o componente da view
+        //define de qual view está pegando a referencia do botao, por isso view.findById
+        Button b = (Button) view.findViewById(R.id.btnOk);
+        //agora trata da forma normal
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // o contexto é obtido de forma diferente
+                //todos os componentes que usam contexto tem que pegar o contexo da forma abaixo
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Estou no fragmento!!!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+        return view;
     }
 
 }
